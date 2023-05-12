eb07f76ffda7f493d597d1b760d1d210f0226796
MuriloEduardo <muriloeduardoooooo@gmail.com>
2023-05-12 20:39:50 -0300

    Stabilize chat response with markdown

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,12 @@
+import ReactMarkdown from 'react-markdown';
 import FormChat from '../components/FormChat';
 import { useContext, useEffect, useState } from 'react';
 import { AuthGoogleContext } from '../contexts/authGoogle';
 
 function Home() {
     const [messages, setMessages] = useState([]);
+    const [responseChat, setResponseChat] = useState('');
+
     const { socket } = useContext(AuthGoogleContext);
 
     const sendMessage = (message) => {
@@ -12,18 +15,28 @@ function Home() {
 
     useEffect(() => {
         if (socket) {
-            socket.onmessage = (event) => {
-                setMessages(messages => [...messages, event.data]);
+            socket.onmessage = ({ data }) => {
+                try {
+                    const { choices } = JSON.parse(data);
+                    const [{ delta: { content } }] = choices;
+
+                    if (content) {
+                        setResponseChat(state => state + content);
+                    }
+                } catch (error) {
+                    console.log(error, data);
+                }
             };
         }
     }, [socket]);
 
     return (
         <>
-            <div className="flex flex-col p-4">
+            <div className="flex flex-col p-4 pb-32 space-y-4">
                 {messages.map((message, index) => (
                     <p key={index}>{message}</p>
                 ))}
+                <ReactMarkdown children={responseChat} className="markdown" />
             </div>
             <FormChat socket={socket} sendMessage={sendMessage} />
         </>
