9395f50b83f303b2950e966f83bc331eb30073a6
MuriloEduardo <muriloeduardoooooo@gmail.com>
2023-05-17 19:28:52 -0300

    Remove vertical padding

--- src/components/FormChat.js
+++ src/components/FormChat.js
@@ -24,7 +24,7 @@ function FormChat({ sendMessage, handleClickSuggestions }) {
     };
 
     return (
-        <div className="p-4 space-y-4">
+        <div className="px-4 space-y-4">
             <h4 className="text-xl text-gray-400">O que você pretende fazer para sua saúde hoje?</h4>
             <Suggestions handleClickSuggestions={handleClickSuggestions} />
             <form onSubmit={handleSubmit} className="flex justify-between bg-slate-800 rounded-xl shadow-md shadow-slate-400">
